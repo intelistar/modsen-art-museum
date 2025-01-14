93037348724f753ee7da4dd73226cbaa9c5a719b
Nikita Garkusha <garkusha500@gmail.com>
2025-01-14 23:47:15 +0300

    feat: add custom hook for managing favorites

---
+++ src/hooks/useFavorites.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect, useCallback } from 'react'
+
+const useFavorites = () => {
+  const getFavoritesFromStorage = (): number[] => {
+    const storedData = sessionStorage.getItem('favorites')
+    return storedData ? JSON.parse(storedData) : []
+  }
+
+  const [favorites, setFavorites] = useState<number[]>(getFavoritesFromStorage)
+
+  const toggleFavorite = useCallback((item: number) => {
+    const currentFavorites = getFavoritesFromStorage()
+
+    const updatedFavorites = currentFavorites.includes(item)
+      ? currentFavorites.filter((favorite) => favorite !== item)
+      : [...currentFavorites, item]
+
+    sessionStorage.setItem('favorites', JSON.stringify(updatedFavorites))
+    setFavorites(updatedFavorites)
+  }, [])
+
+  useEffect(() => {
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === 'favorites') {
+        setFavorites(getFavoritesFromStorage())
+      }
+    }
+
+    window.addEventListener('storage', handleStorageChange)
+    return () => {
+      window.removeEventListener('storage', handleStorageChange)
+    }
+  }, [])
+
+  return [favorites, toggleFavorite]
+}
+
+export default useFavorites
