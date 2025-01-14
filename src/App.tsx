33ec503af1f22ea660e4e7688ad3509e150ee604
Nikita Garkusha <garkusha500@gmail.com>
2025-01-14 18:31:59 +0300

    feat: add routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from '@pages/Home'
+import Layout from '@components/Layout'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <div>404</div>,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+])
+
 function App() {
-  return <>Hello, World!</>
+  return <RouterProvider router={router} />
 }
 
 export default App
